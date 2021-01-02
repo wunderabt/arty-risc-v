155d2d41f4c187dd6938af10c67eaebdaa77fe9f
Uwe Brandt <wunderabt@yahoo.co.in>
2021-01-02 19:21:32 +0100

    progress with tools environment

--- docker/xilinx/run.sh
+++ docker/xilinx/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -u 1000 -it troi.fritz.box/risc-v:latest bash
+docker run --rm \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v /:/host \
+           -e DISPLAY \
+           -u $(id -u) \
+           -it \
+           troi.fritz.box/risc-v:latest bash
