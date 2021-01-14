ebb7ab84217ff3164313853fde4ea4ef45033f6e
Uwe Brandt <wunderabt@yahoo.co.in>
2021-01-14 19:20:34 +0100

    small fix in Dockerfile, adding Arty A7 constraint repo from digilent

--- docker/xilinx/run.sh
+++ docker/xilinx/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker run --rm \
+           --network host \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            -v /:/host \
            -e DISPLAY \
            -u $(id -u) \
            -e HOME=/host${HOME} \
            -it \
-           troi.fritz.box/risc-v:latest bash
+           troi.fritz.box/risc-v:latest /bin/bash
---
+++ vivado_arty_getting_started/readme.md
@@ -0,0 +1,2 @@
+first steps with Arty A7 based on
+https://reference.digilentinc.com/vivado/getting_started/start
