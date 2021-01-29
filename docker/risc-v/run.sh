067369b83e7ba44c0eea6e48e02c8de1c80439d3
Uwe Brandt <wunderabt@yahoo.co.in>
2021-01-29 21:15:36 +0100

    add python2 to fix sifive rom generation (empty with py3) and give docker user usb access

--- docker/risc-v/run.sh
+++ docker/risc-v/run.sh
@@ -2,9 +2,16 @@
 
 image_name="troi.fritz.box/risc-v:latest"
 
+# HINT: on the host the following group access rights were set:
+#       /dev/ttyUSB* - dialout
+#       /dev/bus/usb/* - root
+#       so the user is added to those groups to allow vivado to
+#       program the FPGA via USB
 docker build --tag ${image_name}_${USER} -<<EOF
 FROM ${image_name}
-RUN useradd --uid $(id -u) --home /host/${HOME} ${USER}
+RUN useradd --uid $(id -u) --home /host/${HOME} ${USER} && \
+    usermod -a -G dialout ${USER} && \
+    usermod -a -G root ${USER}
 EOF
 
 docker run --rm \
